cdae661abf5926371b8a480dfdfee68b6d07c01c
l00624481 <longyun13@huawei.com>
2022-08-04 11:53:54 +0800

    chore: add generated files to version control

---
+++ packages/icons/src/generated/SmilingFace.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as Svg from 'react-native-svg';
+export default (props?: Svg.SvgProps): React.ReactElement<Svg.SvgProps> => (
+<Svg.Svg {...props}  viewBox="0 0 24 24"><Svg.Defs><style></style></Svg.Defs><Svg.G id="Layer_2" data-name="Layer 2"><Svg.G id="smiling-face"><Svg.G id="smiling-face" data-name="smiling-face"><Svg.Rect width="24" height="24" opacity="0"/><Svg.Path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm5 9a5 5 0 0 1-10 0z" id="🎨-Icon-Сolor"/></Svg.G></Svg.G></Svg.G></Svg.Svg>
+);
